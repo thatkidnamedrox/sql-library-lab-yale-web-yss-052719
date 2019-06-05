b0aef5ff91c4d6346e43f59adea09897e9b33ff7
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-05 17:18:30 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -47,6 +47,7 @@ def select_series_title_with_most_human_characters
   GROUP BY series.title
   HAVING characters.species = 'human'
   ORDER BY COUNT(character_books.character_id)
+  ;
   """
 end
 
@@ -57,6 +58,6 @@ def select_character_names_and_number_of_books_they_are_in
   INNER JOIN character_books
   ON characters.id = character_books.character_id
   GROUP BY characters.name
-
+  ;
   """
 end
