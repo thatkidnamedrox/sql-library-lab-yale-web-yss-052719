47f064f1221fd184607e003c3bfc4ae2210e1400
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-05 17:19:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -58,6 +58,7 @@ def select_character_names_and_number_of_books_they_are_in
   INNER JOIN character_books
   ON characters.id = character_books.character_id
   GROUP BY characters.name
+  ORDER BY COUNT(character_books.book_id) DESC
   ;
   """
 end
