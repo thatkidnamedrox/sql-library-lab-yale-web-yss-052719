a8b38f78a626cb212c779c56efcce1f081c4ab9a
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-05 17:12:45 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,14 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  """
+  SELECT series.title
+  FROM series
+  INNER JOIN books, character_books
+  ON series.id = books.series_id AND books.id = character_books.book_id
+  GROUP BY series.title
+  ORDER BY COUNT(character_books.character_id)
+  """
 end
 
 def select_character_names_and_number_of_books_they_are_in
